6ad42db7f590c0701f72102289c011399497dc69
Devnith Tissera <devnithtissera@gmail.com>
2024-09-14 23:21:40 +0530

    adding an item to the db

--- app.py
+++ app.py
@@ -1,10 +1,70 @@
-from flask import Flask, render_template
+from datetime import datetime
+from sqlalchemy import Boolean, Column
+from sqlalchemy.orm import Mapped, mapped_column
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///taskly.db"
+db.init_app(app)
 
 
-@app.route('/')
+# class User(db.Model, UserMixin):
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+#     username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+#     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+#     password: Mapped[str] = mapped_column(String(80), nullable=False)
+#     tasks: Mapped[list['Task']] = relationship('Task', back_populates='user')
+#
+#     def __repr__(self):
+#         return f'<User {self.username}>'
+
+
+class Task(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    time: Mapped[str] = mapped_column(String(80), nullable=False)
+    date: Mapped[str] = mapped_column(String(80), nullable=False)
+    done: Mapped[bool] = mapped_column(Boolean, default=False)
+    # user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    # user: Mapped['User'] = relationship('User', back_populates='tasks')
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        date = request.form.get('date')
+        time = request.form.get('time')
+        title = request.form.get('title')
+
+        new_task = Task(title=title, date=date, time=time)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('index.html')
+
+
+@app.route('/submit_todo')
+def submit_todo():
     return render_template('index.html')
 
 
